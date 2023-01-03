3bf207be1f1cdf2157d35ae1ad501328357ce6a9
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2023-01-03 07:40:25 +0545

    SSR Setup

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const PORT = 4000;
 const app = express();
 const productRouter = require('./routes/productRoutes')
 const cors = require('cors')
+const hbs = require('hbs')
+const path = require('path')
 
 app.use(cors())
 
 //Setup which view engine to use
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
 //If you want to change the name of views directory
-app.set('views', './templates');
+app.set('views', './templates')
 //To access the static files in server
 app.use(express.static('./public'))
+//Indicate where to search for partials
+hbs.registerPartials(path.join(__dirname, '/templates/partials'))
 
 app.use('/products', productRouter)
 
